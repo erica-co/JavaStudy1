c08b190ffcd7d89b56866c143981b4a6d0978fb5
erica-co <qkdxks0525@naver.com>
2025-01-03 11:04:21 +0900

    1

---
+++ src/com/winter/app/AppMain.java
@@ -0,0 +1,10 @@
+package com.winter.app;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/com/winter/app/students/Student.java
@@ -0,0 +1,13 @@
+package com.winter.app.students;
+
+public class Student {
+		
+		private String name;
+		private int num;
+		private int kor;
+		private int eng;
+		private int math;
+		private int total;
+		private double avg;
+
+}
